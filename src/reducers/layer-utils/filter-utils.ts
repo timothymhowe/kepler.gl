a9a8aa4221ffdbd690989bbcad0d6a3171aa1b3e
Daria Terekhova <daria.terekhova@actionengine.com>
2022-08-01 16:21:06 +0300

    Review changes
    Signed-off-by: Daria Terekhova <daria.terekhova@actionengine.com>

--- src/reducers/layer-utils/filter-utils.ts
+++ src/reducers/layer-utils/filter-utils.ts
@@ -57,6 +57,7 @@ import KeplerTable, {
 } from 'reducers/table-utils/kepler-table';
 import {DataContainerInterface} from 'reducers/table-utils/data-container-interface';
 import {LAYER_TYPES, getCentroid} from '@kepler.gl/layers';
+import { isValidFilterValue } from '@kepler.gl/utils';
 
 export type FilterResult = {
   filteredIndexForDomain?: number[];
@@ -823,37 +824,6 @@ export function getTimeWidgetHintFormatter(domain: [number, number]): string | u
     : diff > durationHour
     ? 'LT'
     : 'LTS';
-}
-
-/**
- * Sanity check on filters to prepare for save
- */
-/* eslint-disable complexity */
-export function isValidFilterValue(type: string | null, value: any): boolean {
-  if (!type) {
-    return false;
-  }
-  switch (type) {
-    case FILTER_TYPES.select:
-      return value === true || value === false;
-
-    case FILTER_TYPES.range:
-    case FILTER_TYPES.timeRange:
-      return Array.isArray(value) && value.every(v => v !== null && !isNaN(v));
-
-    case FILTER_TYPES.multiSelect:
-      return Array.isArray(value) && Boolean(value.length);
-
-    case FILTER_TYPES.input:
-      return Boolean(value.length);
-
-    case FILTER_TYPES.polygon:
-      const coordinates = get(value, ['geometry', 'coordinates']);
-      return Boolean(value && value.id && coordinates);
-
-    default:
-      return true;
-  }
 }
 
 export function getFilterPlot(
--- test/node/reducers/vis-state-test.js
+++ test/node/reducers/vis-state-test.js
@@ -34,13 +34,8 @@ import {INITIAL_VIS_STATE, DEFAULT_ANIMATION_CONFIG} from 'reducers/vis-state-up
 import {serializeLayer} from 'reducers/vis-state-merger';
 import {processCsvData, processGeojson} from '@kepler.gl/processors';
 import {defaultInteractionConfig} from 'reducers/vis-state-updaters';
-import {
-  Layer,
-  KeplerGlLayers,
-  getDefaultFilter,
-  createNewDataEntry,
-  maybeToDate
-} from '@kepler.gl/layers';
+import {Layer, KeplerGlLayers} from '@kepler.gl/layers';
+import {getDefaultFilter, createNewDataEntry, maybeToDate} from 'reducers/layer-utils';
 import {createDataContainer} from '../../../src/reducers/table-utils';
 import {
   ALL_FIELD_TYPES,
