005986f50a60f78a7e1593ddaec661c4af0d7666
Daria Terekhova <daria.terekhova@actionengine.com>
2022-08-01 16:25:48 +0300

    cloud-provider isolation

--- src/components/modals/overwrite-map-modal.tsx
+++ src/components/modals/overwrite-map-modal.tsx
@@ -24,10 +24,9 @@ import {CenterVerticalFlexbox} from 'components/common/styled-components';
 import {UploadAnimation} from './status-panel';
 import ImageModalContainer from './image-modal-container';
 import {FormattedMessage} from '@kepler.gl/localization';
-import {Provider} from 'cloud-providers';
-
 import {ImageModalContainerProps} from './image-modal-container';
 import {cleanupExportImage} from 'actions';
+import { Provider } from '@kepler.gl/cloud-providers';
 
 /** @typedef {import('./overwrite-map-modal').OverwriteMapModalProps} OverwriteMapModalProps */
 
